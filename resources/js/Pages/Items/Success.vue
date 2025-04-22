bdb5004900ca31b639aebbc7e9d71395293f7434
ethandevelter <ethan.develter@gmail.com>
2025-04-22 20:46:01 +0200

    [UPDATED] Success

--- resources/js/Pages/Items/Success.vue
+++ resources/js/Pages/Items/Success.vue
@@ -1,37 +1,52 @@
 <template>
     <Container>
-        <div>
+      <div>
         <h1>Bedankt voor je aankoop!</h1>
         <div>Order nummer: {{ current_id }}</div>
+  
         <div v-if="orders">
-            <div>Bestelling ID: {{ orders.id }}</div>
-
-            <div>
+          <div>Bestelling ID: {{ orders.id }}</div>
+  
+          <div>
             <h2>Items:</h2>
             <ul>
-                <li v-for="(item, index) in orders.items" :key="index">
+              <li v-for="(item, index) in parsedItems" :key="index">
                 {{ item.name }} (x{{ item.counter }}) - €{{ item.price }} per stuk, totaal: €{{ item.line_total }}
-                </li>
+              </li>
             </ul>
-            </div>
-
-            <div><strong>Totaalbedrag: €{{ orders.total }}</strong></div>
+          </div>
+  
+          <div><strong>Totaalbedrag: €{{ orders.total }}</strong></div>
         </div>
-        </div>
+      </div>
     </Container>
-</template>  
-<script>
-import Container from '../../Components/Container.vue';
-import AppLayout from '@/Layouts/AppLayout.vue';
-
-export default {
+  </template>
+  
+  <script>
+  import Container from '../../Components/Container.vue';
+  import AppLayout from '@/Layouts/AppLayout.vue';
+  
+  export default {
     props: {
-        current_id: Object,
-        orders: Object
+      current_id: Object,
+      orders: Object
     },
     components: {
-        Container,
-        AppLayout,
+      Container,
+      AppLayout
     },
-}
-</script>
+    computed: {
+      parsedItems() {
+        if (typeof this.orders?.items === 'string') {
+          try {
+            return JSON.parse(this.orders.items);
+          } catch (e) {
+            console.error("Invalid JSON in orders.items:", e);
+            return [];
+          }
+        }
+        return this.orders?.items || [];
+      }
+    }
+  }
+  </script>
