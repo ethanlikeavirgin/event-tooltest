9003246efe517463ae300e146dc8733c876ef701
ethandevelter <ethan.develter@gmail.com>
2025-04-22 20:30:51 +0200

    [UPDATED] Success and Build

--- resources/js/Pages/Items/Success.vue
+++ resources/js/Pages/Items/Success.vue
@@ -4,12 +4,9 @@
             <h1>Bedankt voor je aankoop!</h1>
             <div>Order nummer: {{ current_id }}</div>
             <div v-if="orders">
-                <div>Order ID: {{ orders.id }}</div>
-                {{ orders.items }}
-                <div v-for="(order, index) in orders.items" :key="index">
-                {{ order.item_id }} - {{ order.name }} - {{ order.price }} - {{ order.counter }} - {{ order.line_total }}
-                </div>
-                <div>Totaal: {{ orders.total }}</div>
+                <div>{{ orders.id }}</div>
+                <div>{{ orders.items }}</div>
+                <div>{{  orders.total }}</div>
             </div>
         </div>
     </Container>
