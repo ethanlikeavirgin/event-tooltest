5fb3b9a4b0c2009172fcbbecd07d57b2614c279a
ethandevelter <ethan.develter@gmail.com>
2025-05-13 20:18:11 +0200

    [UPDATED] dd test

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -97,8 +97,8 @@ class CartController extends Controller
         $my_orders = [];
         if($orders->guest_token) {
             $my_orders = DB::table('orders')->where('id', $current_id)->where('guest_token', $guest_token)->first();
-            dd($my_orders);
         }
+        dd($guest_token . $my_orders . $current_id);
         /*____UPDATE THE STATS____*/
         if($current_id) {
             DB::table('orders')->where('id', $current_id)->update([
